3a901e213a7b0b296264f11a4000a4f8e1624bd8
wernher-camp <158773287+wernher-camp@users.noreply.github.com>
2025-11-19 17:23:36 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -14,7 +14,11 @@ async function cargar() {
                 <h3>${p.nombre}</h3>
                 <p>${p.descripcion}</p>
                 <p><b>$${p.precio}</b> | Stock: ${p.stock}</p>
+
                 <button onclick="eliminar(${p.id})">Eliminar</button>
+                <button onclick="editarProducto(${p.id}, '${p.nombre}', '${p.descripcion}', ${p.precio}, '${p.imagen_url}', ${p.stock})">
+                    Editar
+                </button>
             </div>
         `;
     });
@@ -35,8 +39,62 @@ async function agregar() {
         body: JSON.stringify(producto)
     });
 
+    limpiarFormulario();
     cargar();
 }
+
+// ----------------------
+//     EDITAR PRODUCTO
+// ----------------------
+
+function editarProducto(id, nombreP, descP, precioP, imgP, stockP) {
+    edit_id.value = id;
+
+    nombre.value = nombreP;
+    descripcion.value = descP;
+    precio.value = precioP;
+    imagen_url.value = imgP;
+    stock.value = stockP;
+
+    btnAgregar.style.display = "none";
+    btnEditar.style.display = "inline-block";
+}
+
+async function guardarEdicion() {
+    const id = edit_id.value;
+
+    const producto = {
+        nombre: nombre.value,
+        descripcion: descripcion.value,
+        precio: precio.value,
+        imagen_url: imagen_url.value,
+        stock: stock.value
+    };
+
+    await fetch(`${API}/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(producto)
+    });
+
+    limpiarFormulario();
+
+    btnAgregar.style.display = "inline-block";
+    btnEditar.style.display = "none";
+
+    cargar();
+}
+
+function limpiarFormulario() {
+    nombre.value = "";
+    descripcion.value = "";
+    precio.value = "";
+    imagen_url.value = "";
+    stock.value = "";
+    edit_id.value = "";
+}
+
+// ----------------------
 
 async function eliminar(id) {
     await fetch(`${API}/${id}`, { method: "DELETE" });
